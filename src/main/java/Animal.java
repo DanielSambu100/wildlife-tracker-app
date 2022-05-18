74bc0fb4d7c3f395806df0e4afa69174a72b3743
DanielSambu100 <daniel.wanyonyi@stdent.moringaschool.com>
2022-05-18 11:29:32 +0300

    update

--- src/main/java/Animal.java
+++ src/main/java/Animal.java
@@ -1,12 +1,17 @@
+import org.sql2o.Connection;
+
+import java.util.Objects;
+
 public class Animal {
+//    encapsulation starts here, private access modifiers
     private String animalName;
     private Integer animalId;
-
+//constructor generated to instantiate the animal class
     public Animal(String animalName, Integer animalId) {
         this.animalName = animalName;
         this.animalId = animalId;
     }
-
+//accessors and mutators to the protected data types declared above.
     public String getAnimalName() {
         return animalName;
     }
@@ -21,5 +26,27 @@ public class Animal {
 
     public void setAnimalId(Integer animalId) {
         this.animalId = animalId;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Animal animal = (Animal) o;
+        return Objects.equals(animalName, animal.animalName) && Objects.equals(animalId, animal.animalId);
+    }
+//overriding the default equals() method
+    @Override
+    public int hashCode() {
+        return Objects.hash(animalName, animalId);
+    }
+    public void save() {
+        try(Connection con = DB.sql2o.open()) {
+            String sql = "INSERT INTO animals (animalName, animalId) VALUES (:animalName, :animalId)";
+            con.createQuery(sql)
+                    .addParameter(String.valueOf(animalId), this.animalId)
+                    .addParameter("animalName", this.animalName)
+                    .executeUpdate();
+        }
     }
 }
--- src/test/java/AnimalTest.java
+++ src/test/java/AnimalTest.java
@@ -5,6 +5,9 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class AnimalTest {
+//    database rule extended here too
+    @Rule
+    public DatabaseRule database = new DatabaseRule();
 
     @BeforeEach
     void setUp() {
@@ -13,7 +16,7 @@ class AnimalTest {
     @AfterEach
     void tearDown() {
     }
-
+//testing parent class instantiation
     @Test
     void Animal_instantiatesCorrectly_true() {
         Animal testAnimal = new Animal("Gazelle", 6);
@@ -23,5 +26,27 @@ class AnimalTest {
     void getName_animalInstantiatesWithId_6() {
         Animal testAnimal=new Animal("Gazelle", 6);
         assertEquals(6,testAnimal.getAnimalId());
+    }
+    @Test
+    public void equals_returnsTrueIfNameAndIdAreSame_true() {
+        Animal firstAnimal = new Animal("Gazelle", 6);
+        Animal anotherAnimal = new Animal("Gazelle", 6);
+        assertTrue(firstAnimal.equals(anotherAnimal));
+    }
+//    testing db setup
+    @Test
+    public void save_insertsObjectIntoDatabase_Person() {
+        Animal testAnimal = new Animal("Gazelle", 6);
+        testAnimal.save();
+        assertTrue(Animal.all().get(0).equals(testAnimal));
+    }
+    @Test
+    public void all_returnsAllInstancesOfAnimal_true() {
+        Animal firstAnimal = new Animal("Gazelle", 6);
+        firstAnimal.save();
+        Animal secondAnimal = new Animal("Fox", 7);
+        secondAnimal.save();
+        assertEquals(true, Animal.all().get(0).equals(firstAnimal));
+        assertEquals(true, Animal.all().get(1).equals(secondAnimal));
     }
 }
