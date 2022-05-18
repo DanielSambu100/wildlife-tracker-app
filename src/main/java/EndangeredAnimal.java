4841abed4bfd382265b7bb0c206653a122b5385b
DanielSambu100 <daniel.wanyonyi@stdent.moringaschool.com>
2022-05-18 15:52:42 +0300

    update EndangeredAnimal class

--- src/main/java/EndangeredAnimal.java
+++ src/main/java/EndangeredAnimal.java
@@ -1,12 +1,17 @@
 public class EndangeredAnimal {
     private String endangeredName;
-    private int animalId;
-//class instantiation
-    public EndangeredAnimal(String endangeredName, int animalId) {
+    private  int animalId;
+    private int endId;
+    private int age;
+    private String health;
+
+    public EndangeredAnimal(String endangeredName, int animalId, int endId, int age, String health) {
         this.endangeredName = endangeredName;
         this.animalId = animalId;
+        this.endId = endId;
+        this.age = age;
+        this.health = health;
     }
-//    getters and setters
 
     public String getEndangeredName() {
         return endangeredName;
@@ -22,5 +27,29 @@ public class EndangeredAnimal {
 
     public void setAnimalId(int animalId) {
         this.animalId = animalId;
+    }
+
+    public int getEndId() {
+        return endId;
+    }
+
+    public void setEndId(int endId) {
+        this.endId = endId;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getHealth() {
+        return health;
+    }
+
+    public void setHealth(String health) {
+        this.health = health;
     }
 }
